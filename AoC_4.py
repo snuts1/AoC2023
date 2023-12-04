1b4a86dd47718f9ad9ce274e3eb4bfc26992e67a
snuts1 <81272174+snuts1@users.noreply.github.com>
2023-12-04 17:03:18 -0500

    Add files via upload

--- AoC_4.py
+++ AoC_4.py
@@ -1,7 +1,7 @@
 import re
 import math
 
-f=open('C:/Users/LabAdmin/Documents/Advent2023/d4/test.txt')
+f=open('C:/Users/steve/Documents/AoC 2023/d4/data.txt')
 data=f.readlines()
 
 #parse lines to 2 lists winners | numbers
@@ -29,31 +29,28 @@ def checker(ws,ns):
 #output=[splitNCheck(line) for line in data]
 #print(sum(output))
 
-### part 2
-#new card list [card#, winner side, my number side, # of copies, ready/done to indicate looping status]
+### part 2 ###
+#new card list [card#, winner side, my number side, # of copies, ready/done to indicate parsing status]
 nuCards=[re.split(r'[:|]',card)+[1]+['ready'] for card in data]
 nuCards=list(enumerate(nuCards))
 def checker2(card):
+    global nuCards #allow writing to global list
     #clean up spaces and nulls
     winners=re.split(r' ',card[1][1])
     mine=re.split(r'[ +\n]',card[1][2])
     mineClean=[0 if x=='' else x for x in mine]
-    copies=card[1][3]
+    copies=nuCards[card[0]][1][3] #for some reason this couldnt read from the input card directly, had to be from nuCards list
     matches=0
-    global nuCards
-    print(card)
+    #check each card and flag when done
     if card[1][4]=='ready':
         for n in mineClean:
             isMatch=False
             isMatch=any(x for x in winners if x==n)
             if isMatch:
                 matches+=1
-        nuCards=[(n,[a,b,c,int(d)+(1),e]) if (matches+card[0])>=n>card[0] else (n,[a,b,c,d,e]) for (n,[a,b,c,d,e]) in nuCards]
+        nuCards=[(n,[a,b,c,int(d)+copies,e]) if (matches+card[0])>=n>card[0] else (n,[a,b,c,d,e]) for (n,[a,b,c,d,e]) in nuCards]
         nuCards=[(n,[a,b,c,d,'done']) if n==card[0]  else (n,[a,b,c,d,e]) for (n,[a,b,c,d,e]) in nuCards]
-    return nuCards
-#checker2(nuCards[2])
-#print(output2)
-#print(nuCards)
+    return nuCards[card[0]][1][3] #output qty of cards
+
 output2=[checker2(line) for line in nuCards]
-output3=[x for (n,[a,b,c,x,e]) in output2[-1]]
-print(output3)
+print(sum(output2))
